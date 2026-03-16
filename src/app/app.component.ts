768b9395e5e0f050b1fd5f6cddcd1cf5d188b6ac
PamelaMBadermann <badermann.pamela@gmail.com>
2026-03-16 12:49:07 -0300

    floor plan component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { MatCardModule } from '@angular/material/card';
-
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HeaderComponent } from "./components/header/header.component";
 
+import { FloorPlanComponent } from './components/floor-plan/floor-plan.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    MatCardModule,
     DragDropModule,
     HeaderComponent,
-    HeaderComponent
+    FloorPlanComponent,
 ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
---
+++ src/app/components/floor-plan/floor-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-floor-plan',
+  imports: [
+    MatCardModule,
+  ],
+  templateUrl: './floor-plan.component.html',
+  styleUrl: './floor-plan.component.css'
+})
+export class FloorPlanComponent {
+
+}
