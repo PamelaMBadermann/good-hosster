f2357a16ba9f96bf8a0f0320b8b37621da4c3ceb
PamelaMBadermann <badermann.pamela@gmail.com>
2026-03-22 17:16:04 -0300

    feat: criação da model table

--- src/app/shared/models/section.model.ts
+++ src/app/shared/models/section.model.ts
@@ -1,11 +1,7 @@
+import { Table } from "./table.model";
+
 export interface Section {
   id: number;
   name: string;
-  setOfTables: Array<{
-    id: number;
-    pax: number;
-    direction: string;
-    status: string;
-    order: number;
-  }>;
+  setOfTables: Array<Table>;
 }
---
+++ src/app/shared/models/table.model.ts
@@ -0,0 +1,7 @@
+export interface Table {
+    id: number;
+    pax: number;
+    direction: string;
+    status: string;
+    order: number;
+}
